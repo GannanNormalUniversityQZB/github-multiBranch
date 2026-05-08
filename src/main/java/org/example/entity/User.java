3d80b286119738979c70372a8cd36880e74d6a83
frontend <1306561049@qq.com>
2026-05-08 10:33:06 +0800

    add:新增实体类User
    - 新增实体类User
    - 作者frontend:邱志斌
    - 创建时间：2026-05-08 10：32
    - 版本：1.0

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,10 @@
+package org.example.entity;
+
+public class User {
+    private Long id;
+    private String userAccount;
+    private String userName;
+    private String password;
+    private String createTime;
+    private String updateTime;
+}
